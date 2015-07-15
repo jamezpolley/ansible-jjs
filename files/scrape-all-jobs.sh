362915826a82e503104eaf48f98c1c0dbc0d1ad8
James Polley <jp@jamezpolley.com>
2015-07-15 15:49:54 +1000

    It Works

--- files/scrape-all-jobs.sh
+++ files/scrape-all-jobs.sh
@@ -22,5 +22,18 @@ set -u
         for file in $OUTPUTDIR/*.html; do
             cp $file $DESTDIR
         done
+
+        cd $DESTDIR
+        (
+            echo "<html><head><title>Stable Job Reports</head></title>"
+            echo "<body>"
+            for report_html in *.html; do
+                report=${report_html%%.html}
+                echo "<p><a href=${report_html}>${report}</a></p>"
+            done
+            echo "</body></html>"
+        ) > index.html
     fi
+
 ) 200> $CONFDIR/scrape-all-jobs.lock
+
