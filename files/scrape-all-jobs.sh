eea0d01b4aca8455194b5d280b2acc4736c9ff55
James Polley <jp@jamezpolley.com>
2015-07-15 13:42:28 +1000

    Add job configs

---
+++ files/scrape-all-jobs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eux
+
+VENVDIR=$1
+CONFDIR=$2
+OUTPUTDIR=${3:-}
+DESTDIR=${4:-}
+
+set +u
+source $VENVDIR/bin/activate
+set -u
+
+(
+    flock --exclusive --nonblock 200 || exit 1
+
+    for conffile in $CONFDIR/*.ini; do
+        scrape_jobs --config-file $conffile --fetch
+    done
+
+    if [ ! -z "$DESTDIR" ]; then
+        for file in $OUTPUTDIR/*.html; do
+            cp $file $DESTDIR
+        done
+    fi
+) 200> $CONFDIR/scrape-all-jobs.lock
